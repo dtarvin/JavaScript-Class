77aa6ce857c016bc2968bc94b40593aea56aa33f
dtarvin <david.tarvin@gmail.com>
2025-08-14 19:44:14 -0500

    work done at home

--- snips_for_slides/example-project/script.js
+++ snips_for_slides/example-project/script.js
@@ -1,4 +1,23 @@
-document.getElementById("hello").addEventListener("click",
-  function () {
-    alert("Hello world!");
-  });
+// document.getElementById("hello").addEventListener("click",
+//   function () {
+//     alert("Hello world!");
+//   });
+
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log(`Number ${i}`);
+// }
+// console.log(`Loop ended`);
+
+
+// while (condition) {
+//   statements to be repeated
+//   every time loop runs
+// }
+
+let letter = prompt("Type the letter 'p'");
+while (letter != "p") {
+  alert("That's not the letter p!");
+  letter = prompt("Try again");
+}
+alert("Good job!");
